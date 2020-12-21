343d6ff2a77f12ad5d6fa8359d58b8b421c49be5
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-21 21:41:08 +0530

    No args handled

--- dact/__main__.py
+++ dact/__main__.py
@@ -5,11 +5,29 @@ from .funcmodule import startDjangoProject
 def main():
     print('in main')
     args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+
+
+    # print('count of args :: {}'.format(len(args)))
+    # for arg in args:
+    #     print('passed argument :: {}'.format(arg))
+    
+    
+    
     if len(args) < 1: 
-        print("Please Enter the Django Project name. Use it like 'dact yourprojectname'")
+        print(
+            
+            """
+            Congratulations! Dact is successfully installed in your system. 
+            To learn more about dact, go to https://ayushmankumar7.github.io/dact/. 
+
+            To get started with Dact, type dact newproject 
+
+            Hurray! Your first Django + React Project is ready to be worked on.
+
+            """
+        )
+
+
     elif len(args) == 1:
 
         startDjangoProject(args[0])
@@ -17,8 +35,8 @@ def main():
     elif len(args) ==2:
         startDjangoProject(args[0], args[1])
 
-    my_object = MyClass('Your Django + reACT Project is ready to use.')
-    my_object.say_name()
+    # my_object = MyClass('Your Django + reACT Project is ready to use.')
+    # my_object.say_name()
 
 def testing():
     print("Testing function was called")
