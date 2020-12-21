5f332371ed70b41233b0ecb8e4cbeea2ebf9abb9
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-21 22:24:02 +0530

    project url manipulation

--- dact/funcmodule.py
+++ dact/funcmodule.py
@@ -12,6 +12,22 @@ def manipulate_settings(setting_file, app_name):
             b.write(line)
      
 
+def manipulate_project_url(pro_url, app_name):
+    with open(pro_url, 'r') as b:
+        lines = b.readlines()
+
+    with open(pro_url, 'w') as b:
+        for i, line in enumerate(lines):
+            if i == 17:
+                b.write('import django.urls import path, include \n')
+            elif i == 21:
+                b.write("\tpath('', include('frontend.urls')),\n")
+
+            else:
+                b.write(line)
+
+
+
 
 
 def startDjangoProject(name, app_name = "reactfrontend"):
