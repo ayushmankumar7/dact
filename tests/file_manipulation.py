2fd8eccf91395e7f8192321a97a77c9eb2aaf268
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-21 13:33:40 +0530

    tests upgraded

--- tests/file_manipulation.py
+++ tests/file_manipulation.py
@@ -3,6 +3,20 @@ import os
 
 print(os.listdir())
 
-for i, line in enumerate(open('testing.py')):
-    print(line)
-    print(i)
+# for i, line in enumerate(open('testing.py', 'a')):
+#     print(line)
+#     print(i)
+
+#     if i == 1:
+
+
+with open('testing.py', 'r') as b:
+    lines = b.readlines()
+
+print(lines)
+with open('testing.py', 'w') as b:
+    for i, line in enumerate(lines):
+        print(i)
+        if i == 2:
+            b.write("\tSomething \n")
+        b.write(line)
--- tests/testing.py
+++ tests/testing.py
@@ -1,2 +1,4 @@
 def something():
     print("er")
+	Something 
+    print("something new")
