9f08672002790d04d7e475caeb6f57ae310ea937
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-21 16:22:59 +0530

    customize app name

--- dact/funcmodule.py
+++ dact/funcmodule.py
@@ -1,24 +1,24 @@
 import os
 
 
-def manipulate_settings(setting_file):
+def manipulate_settings(setting_file, app_name):
     with open(setting_file, 'r') as b:
         lines = b.readlines()
 
     with open(setting_file, 'w') as b:
         for i, line in enumerate(lines):
             if i== 39:
-                b.write("\t'reactfrontend.apps.ReactfrontendConfig',\n")
+                b.write(f"\t'{app_name}.apps.{app_name.title()}Config',\n")
             b.write(line)
      
 
 
 
-def startDjangoProject(name):
+def startDjangoProject(name, app_name = "reactfrontend"):
     print("[INFO] Creating Django Project ....")
     os.system(f"django-admin startproject {name}")
     os.chdir(name)
     print("[INFO] Creating React App ....")
-    os.system("django-admin startapp reactfrontend")
+    os.system(f"django-admin startapp {app_name}")
     os.chdir(name)
-    manipulate_settings('settings.py')
+    manipulate_settings('settings.py', app_name)
