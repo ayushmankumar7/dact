0c98bdab3927d678aa34237277aea47dd1a36733
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-22 21:30:21 +0530

    added webpack.config.js generator method and more

--- dact/createreact.py
+++ dact/createreact.py
@@ -14,8 +14,94 @@ def make_dirs(x):
     os.chdir("..")
     os.chdir(x)
     os.mkdir("templates")
-    os.chdir("templates")
-    
+
+def make_template():
+    open("index.html",'x')
+    with open("index.html",'r+') as file:
+        file.write('''
+        {%load static%}
+        <html lang="en">
+            <body>
+                <div id="root"></div>
+            </body>
+            <script src = "{% static '/dist/app.bundle.js' %}"></script>
+        </html>
+        ''')
+        file.close()
+
+def write_webpack_config():
+    open('webpack.config.js','x')
+    with open('webpack.config.js','r+') as file:
+        file.write(
+            '''
+            const path = require("path");
+            var webpack = require('webpack');
+            var BundleTracker = require('webpack-bundle-tracker');
+
+
+            module.exports = {
+                entry:{ app:'./src/index.js'},
+                watch:true,
+                devtool:'source-map',
+                output: {
+                path: path.resolve(__dirname,'dist'),
+                filename: "[name].bundle.js",
+            },
+
+            plugins: [
+                new BundleTracker({filename: './webpack-stats.json'}),
+            ],
+            module: {
+            rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use: ['babel-loader'], 
+            },
+            {
+                test: /\.svg$/,
+                use: ['react-svg-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(otf|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'url-loader'
+                }]
+            }
+            ]
+            },
+  
+            resolve: {
+                extensions: ['*', '.js', '.jsx','.scss']
+            }
+            };
+            '''.strip()
+        )
+        file.close()
+
+def write_react_app():
+    open("App.js",'x')
+    with open('App.js','w') as file:
+        file.write(
+            '''
+            import React from 'react';
+            
+            '''
+        )
 
 def create():
     x = getcurrent()
