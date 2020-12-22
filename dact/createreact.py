b01e7025b46a9449bc86532af7b8c8389b19c7df
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-22 16:49:49 +0530

    adding static dirs

--- dact/createreact.py
+++ dact/createreact.py
@@ -7,7 +7,19 @@ def getcurrent():
 
 def make_dirs(x):
     os.mkdir("static")
+    os.chdir("static")
+    os.mkdir("src")
+    os.mkdir("js")
+    os.mkdir("css")
+    os.chdir("..")
+    os.chdir(x)
     os.mkdir("templates")
+    os.chdir("templates")
+    
+
+def create():
+    x = getcurrent()
+    make_dirs(x)
 
      
 
