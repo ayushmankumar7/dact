c9914fa1217beb47f7de004044d7a219be480af8
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-23 11:12:15 +0530

    setup file configured

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'dact',
     version = '0.1.0',
+    author = "Ayushman Kumar|Arkaprabha Chakraborty",
+    author_email="ayushmankumar7@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ayushmankumar7/dact",
     packages = ['dact'],
     entry_points = {
         'console_scripts': [
             'dact = dact.__main__:main', 
             'dact-test = dact.__main__:testing'
         ]
-    })
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+
+    
+    )
