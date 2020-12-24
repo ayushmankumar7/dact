acf556e296f6ba3f77bc1c9984718c7096ff96c0
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-24 15:20:50 +0530

    cli visuals

---
+++ dact/visuals.py
@@ -0,0 +1,36 @@
+def complete_msg(app_name):
+    print("""
+                                                                                                 
+                                                                 `+:.                               
+                                                                 .:oso/.                            
+                                                                    `:+so/-                         
+                  s.                                             +oo+-``:+ss/-`                     
+                  s.                            `/               oso+ss+:``-/ss+-`                  
+          -//////:s.   .:::::/:.      -:/::::-  .s:::-           oso  -/os+:. -/os+:                
+        .o:`     `s.          `o-   .o:`        .s               oso     ./oso:` .:.    `-:         
+        o:        s.      ```` //   o:          .s               oso        .:/     .:/osso-        
+        o-        s.   :/::--::o/   s-          .s               oso           `-/+sss+:-`          
+        /+        s.  :+       //   +/          .s               oso       .:/osso/:.  .:/          
+        `/+-`   `.s.  .o-`   `-o/   `/+-`    `  `o-              oso  `-:+sss+:-` `-/osso/.         
+          `-:::::-.    `-::::-``-     `-:::::-   `-::-           oso/osso/:.  .:+oso+:.             
+                                                                 oss+/-` `-/+sso/-`                 
+                                                                     .:/oss+:.`                     
+                                                                 .:+sso/-.                          
+                                                                 .+:-`                              
+                                                                                                    
+                                                                                         
+    
+    
+     """)
+
+    print("Setup Complete! ")
+    print("""
+        To Start the sever write the following commands: 
+            1. cd %s
+            2. python manage.py runserver (Windows)
+               python3 manage.py runserver (Others)
+
+        HAPPY HACKING!! 
+     """%(app_name))
+
+
