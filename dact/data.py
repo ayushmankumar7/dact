6493d2c94f6dd40da8160125ace512afd16c7e65
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-27 17:22:47 +0530

    Added app.css

--- dact/data.py
+++ dact/data.py
@@ -51,4 +51,20 @@ return(
 export default App;
         '''.strip()
         self.app_css = '''
+.App
+{
+    text-align: center;
+    color:black;
+}
+.Applogo
+{
+    max-width: 100%;
+    height: 50%;
+    width: 90%; 
+}
+.Apptext
+{
+    font-family: sans-serif;
+    color:#169A9A;
+}
         '''
