471ed3ab6b5b381026d99851805fcdbc36d579ad
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-27 16:44:03 +0530

    app.js template code added to dataclass

--- dact/data.py
+++ dact/data.py
@@ -8,7 +8,7 @@ class Data:
             </body>
             <script src = "{% static '/dist/app.bundle.js' %}"></script>
         </html>
-        '''
+        '''.strip()
         self.index_css = '''
         '''
         self.index_js = '''
@@ -20,4 +20,35 @@ ReactDOM.render(
             <App />,
             document.getElementById('root')
 );
+        '''.strip()
+        self.app_js = '''
+import React from 'react';
+import './App.css';
+import Dact from './dact.svg';
+
+const App = () => {
+return(
+        <div className="App">
+            <Dact className="Applogo"/>
+            <div className="Apptext">
+                <br></br>    
+                <h4>It's Up and Running !!!</h4>
+                <h6>
+                    <p>
+                    Edit App.js to get started with frontend!!
+                    </p>
+                    <p>
+                    Server Running at 127.0.0.1:8000
+                    </p>
+                    <p>
+                        Run dact-watch in BASE_DIR to start dev mode
+                    </p>
+                </h6>
+            </div>
+        </div>
+        );
+}
+export default App;
+        '''.strip()
+        self.app_css = '''
         '''
