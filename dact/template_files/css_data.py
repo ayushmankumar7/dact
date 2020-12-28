8fce0a88166d557212b2eea647a3dac6b6b54f14
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-28 20:56:56 +0530

    added indes.css modifiactions css_templates

--- dact/template_files/css_data.py
+++ dact/template_files/css_data.py
@@ -1,3 +1,4 @@
 class Css_Templates():
     def __init__(self):
-        self.index_css:'''.body{margin:0;} '''
+        self.index_css:'''.body{margin:0;} 
+        .body --webkit {--sroll:null;}'''
