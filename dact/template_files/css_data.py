34b9f0a52ae12428634e7fc4d984abcf50a1ec87
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-28 20:58:51 +0530

    added app.css to css_templates

--- dact/template_files/css_data.py
+++ dact/template_files/css_data.py
@@ -1,3 +1,22 @@
 class Css_Templates():
     def __init__(self):
         self.index_css:''' '''
+        self.app_css:'''
+
+.App
+{
+    text-align: center;
+    color:black;
+}
+.Applogo
+{
+    max-width: 100%;
+    height: 50%;
+    width: 90%; 
+}
+.Apptext
+{
+    font-family: sans-serif;
+    color:#169A9A;
+}
+        '''.strip()
