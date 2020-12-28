d01982e8aba009497df8e8dead38f5e57c171fc5
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-28 21:44:07 +0530

    index.html added

--- dact/template_files/html_data.py
+++ dact/template_files/html_data.py
@@ -1,2 +1,13 @@
 class Html_Templates():
-    
+    def __init__(self):
+        self.index_html = '''
+         {%load static%}
+        <html lang="en">
+            <body>
+                <div id="root"></div>
+            </body>
+            <script src = "{% static '/dist/app.bundle.js' %}"></script>
+        </html>
+        '''.strip()
+        self.dact_svg = '''
+        '''
