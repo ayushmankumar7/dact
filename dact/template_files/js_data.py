da6ff28f5233a9df938f31f72c64bd8d83ca491e
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-12-28 20:39:30 +0530

    added webpack.config.js to refactored files

--- dact/template_files/js_data.py
+++ dact/template_files/js_data.py
@@ -39,3 +39,72 @@ return(
 }
 export default App;
         '''.strip()
+        self.webpack_config_js = '''
+        const path = require("path");
+            var webpack = require('webpack');
+            var BundleTracker = require('webpack-bundle-tracker');
+
+
+            module.exports = {
+                entry:{ app:'./src/index.js'},
+                devtool:'source-map',
+                output: {
+                path: path.resolve(__dirname,'dist'),
+                filename: "[name].bundle.js",
+            },
+
+            plugins: [
+                new BundleTracker({filename: './webpack-stats.json'}),
+            ],
+            module: {
+            rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use: ['babel-loader'], 
+            },
+            {
+                test: /\.svg$/,
+                use: ['react-svg-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(otf|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'url-loader'
+                }]
+            }
+            ]
+            },
+            resolve: {
+                extensions: ['*', '.js', '.jsx','.scss']
+            }
+            };
+        '''.strip()
