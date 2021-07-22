9e7d84a2f110a127274a83b0aa60e09c89bbf8cf
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-07-22 22:24:33 +0530

    Version 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'dact',
-    version = '1.2.3',
+    version = '1.3.0',
     author = "Ayushman Kumar|Arkaprabha Chakraborty",
     author_email="ayushmankumar7@gmail.com",
     long_description = long_description,
